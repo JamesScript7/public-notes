795de0b678e00c32fae27d60b9ff77da8d3dce6f
James Kim <yameskim@gmail.com>
2017-05-20 23:49:18 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBCLrczRKq3OYVlkHbyvrDkUTKeznP9onI",
+    authDomain: "my-project-f6815.firebaseapp.com",
+    databaseURL: "https://my-project-f6815.firebaseio.com",
+    projectId: "my-project-f6815",
+    storageBucket: "my-project-f6815.appspot.com",
+    messagingSenderId: "477427081213"
+  };
+  firebase.initializeApp(config);
+  console.log(firebase);
+
+
+})();
